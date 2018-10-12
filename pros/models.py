533746349b2986a1196a328137f092af9243afce
DK-denno <dennisveer27@gmail.com>
2018-10-12 15:03:02 +0300

    added new values to profile and finished up on the posts model

--- pros/models.py
+++ pros/models.py
@@ -10,6 +10,7 @@ class Profile(models.Model):
     user = models.OneToOneField(User,on_delete=models.CASCADE,related_name='profile')
     dp =  models.ImageField(upload_to='images')
     bio = HTMLField(max_length=500)
+    phone_number = models.IntegerField(max_length=10)
 
     
     def save_profile(self):
@@ -21,6 +22,44 @@ class Profile(models.Model):
     def __str__(self):
         return self.user.username
 
+
 class Posts(models.Model):
     user = models.ForeignKey(User,on_delete=models.CASCADE,related_name='posts')
+    caption = models.CharField(max_length=250)
+    image = models.ImageField(upload_to='posts')
+    postedon = models.DateTimeField(auto_now_add=True)
+    
+    
+    def __str__(self):
+        return self.user.username
 
+    class Meta:
+        ordering = ['-id']
+    
+    @classmethod
+    def save_post(self):
+        self.save()
+    @classmethod
+    def get_posts(cls):
+         posts = cls.objects.all()
+         return posts
+    @classmethod
+    def delete_post(self):
+        self.delete()
+    
+    def save_image(self):
+        self.save()
+
+    @classmethod
+    def delete_image_by_id(cls, id):
+        pictures = cls.objects.filter(pk=id)
+        pictures.delete()
+
+    @classmethod
+    def get_image_by_id(cls, id):
+        pictures = cls.objects.get(pk=id)
+        return pictures
+
+
+    
+    
