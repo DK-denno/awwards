3c85c661fb19174b4622573d35d939279efbf5fe
DK-denno <dennisveer27@gmail.com>
2018-10-12 14:27:30 +0300

    added form to update profile

--- pros/forms.py
+++ pros/forms.py
@@ -1,7 +1,7 @@
 from django import forms
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.models import User
-
+from .models import Profile
 class SignUpForm(UserCreationForm):
     first_name = forms.CharField(max_length=40)
     last_name = forms.CharField(max_length=40)
@@ -10,3 +10,9 @@ class SignUpForm(UserCreationForm):
         model = User
         exclude = []
         fields = ['first_name','last_name','username','email','password1','password2']
+
+class ProfileForm(forms.ModelForm):
+    class Meta:
+        model = Profile
+        exclude = ['user']
+        fields = ['dp','bio']
