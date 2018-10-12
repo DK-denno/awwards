bf3843e6695e2f142d83c73c1c7cb7b0790a65b6
DK-denno <dennisveer27@gmail.com>
2018-10-12 15:06:57 +0300

    migrated the posts model

--- pros/models.py
+++ pros/models.py
@@ -10,8 +10,7 @@ class Profile(models.Model):
     user = models.OneToOneField(User,on_delete=models.CASCADE,related_name='profile')
     dp =  models.ImageField(upload_to='images')
     bio = HTMLField(max_length=500)
-    phone_number = models.IntegerField(max_length=10)
-
+    phone_number = models.BigIntegerField()
     
     def save_profile(self):
         self.save()
